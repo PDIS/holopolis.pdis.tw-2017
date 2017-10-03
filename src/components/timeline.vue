b3077f68054b76cd2f6e574b1cb394f010481b36
XanxusX <tako30215@hotmail.com>
2017-10-03 10:42:03 +0800

    add timeline link

--- src/components/timeline.vue
+++ src/components/timeline.vue
@@ -9,7 +9,7 @@
       <div class="cd-timeline-content">
         <h2>{{timeline.title}}</h2>
         <div v-html="timeline.content"></div>
-        <a href="#0" class="cd-read-more">Read more</a>
+        <a v-bind:href="timeline.link" class="cd-read-more">Read more</a>
         <span class="cd-date">{{timeline.date}}</span>
       </div>
       <!-- cd-timeline-content -->
@@ -40,17 +40,16 @@ export default {
       let topics = res.data.topic_list.topics.map(topic => topic.id)
       topics = topics.slice(1)
       topics.map(topic => {
-        console.log(topics)
         if (topic != '3960') {
           axios.get('https://talk.pdis.nat.gov.tw/t/' + topic + ".json").then(res => {
             let timeline = {}
             //console.log(data)
             timeline.title = res.data.title
-            timeline.content = res.data.post_stream.posts[0].cooked
+            let ct = res.data.post_stream.posts[0].cooked.split('<hr>')
+            timeline.content = ct[0]
             timeline.link = res.data.post_stream.posts[0].link_counts[0].url
             timeline.date = new Date(res.data.post_stream.posts[0]['created_at'].toString()).toISOString().substring(0, 10);
             this.timelines.push(timeline)
-            console.log(this.timelines)
           })
         }
       })
