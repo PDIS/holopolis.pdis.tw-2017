e9a5702f05c6ed4cae67fb5c3797af4de711fe09
XanxusX <tako30215@hotmail.com>
2017-10-06 17:41:58 +0800

    add cname

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      <p class="m-0 text-center text-white">Made by PDIS</p>
     </div>
   </footer>
 </template>
