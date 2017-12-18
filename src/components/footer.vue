59b356dbfc99a2a2887de0d93c80094d6af7a98a
XanxusX <tako30215@hotmail.com>
2017-12-18 15:21:50 +0800

    change to html+vue

--- src/components/footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Made by PDIS<i class="fa fa-github"></i></p>
-    </div>
-  </footer>
-</template>
-
